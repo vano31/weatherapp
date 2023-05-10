a01826e737fff26c96226f4267ec19340d0ec3da
vano31 <vanosike31@gmail.com>
2023-05-10 19:30:08 -0400

    Add Asset management steps to webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert('hello weather app!')
+import './style.css';
+
+let hello = document.createElement('h1');
+
+hello.textContent = 'Hello Citizens of Planet Earth';
+
+hello.classList.add('hello');
+
+document.body.appendChild(hello);
