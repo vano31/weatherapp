1fd6550a1894ec5b32cc9dbb808b242fd57e03df
vano31 <vanosike31@gmail.com>
2023-05-27 15:31:04 -0400

    Add all-the-cities npm module, working on random button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {asyncWeather} from './promisedata.js';
 import {fullPage, menuBar, searchSection, buttonSection, dataSection, dataSectionTop, dataSectionBottom, mapSection, searchBar, searchButton, locationHeading, currentTemperature, conditionIcon, lastUpdated, temperatureSwitchButton, conditionInfoHeading, uvBox, humidityBox, windBox, cloudBox, randomButton, refreshButton} from './structure.js';
 import './style.css';
+const cities = require('all-the-cities');
 
 
 /*
@@ -219,5 +220,62 @@ refreshButton.addEventListener('click', function() {
 
 })
 
+randomButton.addEventListener('click', function() {
+
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    }
+
+    let randomNumber = getRandomIntInclusive(0, 138398);
+
+    currentWeather.name = cities[randomNumber].name;
+
+    asyncWeather(currentWeather.name).then(function(response) {
+        //console.log(response);
+        currentWeather.officialName = response.location.name;
+        currentWeather.region = response.location.region;
+        currentWeather.country = response.location.country;
+        currentWeather.lat = response.location.lat;
+        currentWeather.lon = response.location.lon;
+        currentWeather.tempF = response.current.temp_f;
+        currentWeather.tempC = response.current.temp_c;
+        currentWeather.conditionText = response.current.condition.text;
+        currentWeather.conditionIcon = 'https://' + ((response.current.condition.icon).split('//'))[1];
+        currentWeather.humidity = response.current.humidity;
+        currentWeather.wind = response.current.wind_mph;
+        currentWeather.cloud = response.current.cloud;
+        currentWeather.uv = response.current.uv;
+        currentWeather.lastUpdated = new Date();
+
+        return currentWeather
+
+    }).then(function(currentWeather) {
+
+        console.log(`Just Checking`);
+        displayCurrentWeatherToDom();
+        saveLastSearched();
+
+        /*
+
+        for (const property in currentWeather  ) {
+            console.log(currentWeather[property]);
+        }
+        */
+        
+        //return currentWeather
+
+    })
+
+    
+    console.log(currentWeather);
+    return currentWeather;
+
+    //displayCurrentWeatherToDom();
+
+
+
+})
 
 
