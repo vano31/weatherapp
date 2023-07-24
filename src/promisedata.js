f61ad603edc51878f0a1b605fb6429c14d3a8043
vano31 <vanosike31@gmail.com>
2023-07-24 16:08:18 -0400

    lets see if this works

--- src/promisedata.js
+++ src/promisedata.js
@@ -4,7 +4,7 @@ function asyncWeather (searchlocation) {
 
         //let searchlocation = 'abuja';
 
-        let attemptedLocation = fetch(`http://api.weatherapi.com/v1/current.json?key=8a9310029e7f43e2b6a40658230205&q=${searchlocation}&aqi=no`, {mode: 'cors'});
+        let attemptedLocation = fetch(`https://api.weatherapi.com/v1/current.json?key=8a9310029e7f43e2b6a40658230205&q=${searchlocation}&aqi=no`, {mode: 'cors'});
 
         attemptedLocation.then(function(response) {
             //console.log(response)
